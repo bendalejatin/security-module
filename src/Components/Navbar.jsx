2019791b69ba0a8177d06b354685b13a294d8f1d
bendalejatin <bendalejatin35@gmail.com>
2025-05-01 22:19:23 +0530

    update profile

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Profile from '@mui/icons-material/AccountCircle';
-import LogoutIcon from '@mui/icons-material/Logout';
 import './styles/Navbar.css'; // Assuming CSS is in App.css; adjust path if needed
 
 const Navbar = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    localStorage.removeItem('guardToken');
-    localStorage.removeItem('guardEmail');
-    alert('✅ Logged out successfully!');
-    navigate('/security/login');
-  };
-
   return (
     <nav className="navbar">
       <div className="navbar-brand">
@@ -21,7 +11,6 @@ const Navbar = () => {
       </div>
       <div className="navbar-links">
         <Link to="/guard-profile" className="navbar-link"><Profile/><p>Guard Profile</p></Link>
-        <button onClick={handleLogout} className="logout-button"><LogoutIcon/> <p>Logout</p></button>
       </div>
     </nav>
   );
