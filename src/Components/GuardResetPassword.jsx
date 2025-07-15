772793be8eff32f96b351a4c757632baf76afd9a
Hitendra213 <22amtics059@gmail.com>
2025-07-15 12:36:54 +0530

    Update GuardResetPassword.jsx

--- src/Components/GuardResetPassword.jsx
+++ src/Components/GuardResetPassword.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import "./styles/SecurityGuardLogin.css";
 
 //const BASE_URL = "http://localhost:5000"; // Adjust this to your backend URL
-const BASE_URL = "https://dec-entrykart-backend.onrender.com" ; // deployment url
+const BASE_URL = "https://entrykart-admin.onrender.com" ; // deployment url
 
 const GuardResetPassword = () => {
   const { token } = useParams(); // get token from URL
